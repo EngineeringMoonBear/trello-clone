0f72df52822153cab348e3fdd4fa7f775d89b2f9
Joshua <yoshiama12@gmail.com>
2015-03-09 14:44:08 -0400

    added scafolding for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309152239) do
+ActiveRecord::Schema.define(version: 20150309183441) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
